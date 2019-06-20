6c43aa5974677ca5b58751e1ba9c746eb367f080
Sam Carrington <sam.carrington@analogfolk.com>
2019-06-20 11:36:15 +0100

    populate changelog

---
+++ pages/changelog.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .container
+    section#changelog(v-html="changelog")
+</template>
+
+<script>
+import changelog from '../CHANGELOG.md'
+
+export default {
+  data() {
+    return {
+      title: 'Stylish AF - Change log'
+    }
+  },
+  computed: {
+    changelog() {
+      return changelog
+    }
+  },
+  head() {
+    return {
+      title: this.title
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
