3614dbb248b2337590e2c47e191efa2fc3950f6b
Sam Carrington <sam.carrington@capgemini.com>
2023-01-06 14:35:39 +0000

    prettier and extend the sitemap generator to include static pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-      '@nuxt/content',
-      '@nuxtjs/tailwindcss',
-      '@nuxtjs/color-mode'
-    ],
-    colorMode: {
-        classSuffix: ''
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  colorMode: {
+    classSuffix: '',
+  },
+  nitro: {
+    prerender: {
+      routes: ['/rss.xml', 'sitemap.xml'],
     },
-    nitro: {
-      prerender: {
-        routes: [
-          '/rss.xml',
-          'sitemap.xml'
-        ]
-      }
+  },
+  runtimeConfig: {
+    public: {
+      appVersion: process.env.npm_package_version,
     },
-    runtimeConfig: {
-      public: {
-        appVersion: process.env.npm_package_version,
-      }
+  },
+  content: {
+    documentDriven: true,
+    navigation: {
+      fields: ['navTitle'],
     },
-    content: {
-        documentDriven: true,
-        navigation: {
-            fields: ['navTitle']
-        },
-        highlight: {
-            // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
-            theme: 'dracula',
-            preload: ['py', 'css', 'scss', 'ts', 'json', 'js']
-        }
-    }
-})
+    highlight: {
+      // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
+      theme: 'dracula',
+      preload: ['py', 'css', 'scss', 'ts', 'json', 'js'],
+    },
+  },
+});
