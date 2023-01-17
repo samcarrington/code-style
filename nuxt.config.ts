a0c4ef7098cb0a3e503dbfcb3ee832f07e14dae3
Sam Carrington <sam.carrington@capgemini.com>
2023-01-17 10:57:49 +0000

    fix pre-render route for sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   },
   nitro: {
     prerender: {
-      routes: ['/rss.xml', 'sitemap.xml'],
+      routes: ['/rss.xml', '/sitemap.xml'],
     },
   },
   runtimeConfig: {
